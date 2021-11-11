837af08ee38e1d65f3fefe566af91c2c621700ef
Govind Kumar <govindsingh6498@gmail.com>
2021-11-11 22:18:13 +0530

    Added:To display uer input name

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import logo from "./assets/img/bridgelabz.png"
 import './App.css';
 import React from 'react';
 class App extends React.Component{
-  url="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glassdoor.co.in%2FLocation%2FBridgeLabz-Solutions-Bengaluru-Location-EI_IE1318507.0%2C20_IL.21%2C30_IC2940587.htm&psig=AOvVaw1Uo3KVtXuCLWRNzjRwTbE-&ust=1636733355414000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiW8IfbkPQCFQAAAAAdAAAAABAD"
+  url="https://www.bridgelabz.com"
   constructor(){
     super()
     this.state={
-      title:"Hello from Bridgelabz!!"
+      userName:""
     }
   }
   onClick=(Sevent)=>{
     console.log("save button is clicked",Sevent);
     window.open(this.url,"_blank");
   }
+  onNameChange=(event)=>{
+    console.log("Value is ",event.target.value);
+    //set the title using setState method
+    this.setState({userName:event.target.value})
+  }
   render(){
     return(
+      <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.userName} from BridgeLabz</h1>
         <img src={logo} onClick={this.onClick}
         alt="The BridgeLabz logo: A bridge to Employeement through lab wokrs"></img>
       </div>
-    )
+      <div>
+        <input onChange={this.onNameChange}></input>
+      </div>
+      </>
+    );
   }
 }
 
