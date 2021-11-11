32f4763d64511141d55fbf3a530ba8ac8d76ed06
Govind Kumar <govindsingh6498@gmail.com>
2021-11-11 23:41:55 +0530

    Added:To fill the content of the page

--- src/App.js
+++ src/App.js
@@ -28,17 +28,38 @@ class App extends React.Component{
   }
   render(){
     return(
-      <div>
-      <div>
+      <>
+      <div class="placeAtCenter">
         <h1>Hello {this.state.userName} from BridgeLabz</h1>
         <img src={logo} onClick={this.onClick}
         alt="The BridgeLabz logo: A bridge to Employeement through lab wokrs"></img>
-      </div>
-      <div>
-        <input onChange={this.onNameChange}></input>
+        </div>
+        <div>
+        <input class="hello" onChange={this.onNameChange}></input>
         <span className="error-output">{this.state.nameError}</span>
       </div>
-      </div>
+      <main class ="content">
+        <div id="article-content">
+          <article>
+            <p>At bridgelabz,we are techie community of</p>
+          </article>
+          <article>
+            <ul>
+              <li>Technologists</li>
+              <li>Thinkers</li>
+              <li>Builder</li>
+            </ul>
+            <p>Working together to keep the teck Employeebility of engineers alive and accesible,so Tech Companies worldwide can get contributors and creators for Technology solutions.We believe this act of human collaboration across an Employability platform is essential
+                to individual growth and our collective future</p>
+          </article>
+          <article>
+            <p>
+            To know about us,visit <a href="https://www.bridgelabz.com/">Bridgelabz</a> to learn even more about our mission i.e <strong> Employability to all</strong>
+            </p>
+          </article>
+        </div>
+      </main>
+      </>
     );
   }
 }
