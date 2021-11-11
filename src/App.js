5cd61f0e31d7ad167e856c56e1272fb645586fb6
Govind Kumar <govindsingh6498@gmail.com>
2021-11-11 22:35:22 +0530

    MODIFIED:To listen and validate name when updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from "./assets/img/bridgelabz.png"
 import './App.css';
 import React from 'react';
 class App extends React.Component{
-  url="https://www.bridgelabz.com"
+  url="https://www.bridgelabz.com/"
   constructor(){
     super()
     this.state={
-      userName:""
+      userName:"",
+      nameError:""
     }
   }
   onClick=(Sevent)=>{
@@ -15,12 +16,19 @@ class App extends React.Component{
   }
   onNameChange=(event)=>{
     console.log("Value is ",event.target.value);
+    const nameRegex =RegExp("[A-Z]{1}[a-zA-Z\\s]{2,}$");
     //set the title using setState method
     this.setState({userName:event.target.value})
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError:""})
+    }
+    else{
+      this.setState({nameError:"Name is Incorrect"})
+    }
   }
   render(){
     return(
-      <>
+      <div>
       <div>
         <h1>Hello {this.state.userName} from BridgeLabz</h1>
         <img src={logo} onClick={this.onClick}
@@ -28,8 +36,9 @@ class App extends React.Component{
       </div>
       <div>
         <input onChange={this.onNameChange}></input>
+        <span className="error-output">{this.state.nameError}</span>
       </div>
-      </>
+      </div>
     );
   }
 }
